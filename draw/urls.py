419b359dda6c302e66b6ff9a7abd0caf81e6e2a4
zding52 <zding52@berkeley.edu>
2019-07-26 02:26:55 -0400

    DONE for now?

--- draw/urls.py
+++ draw/urls.py
@@ -5,6 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('largescreen/',views.largescreen, name='largescreen'),
+    path('1/',views.one, name='1'),
+    path('2/',views.two, name='2'),
+    path('3/',views.three, name='3'),
+    path('4/',views.four, name='4'),
+    path('5/',views.five, name='5'),
+    path('6/',views.six, name='6'),
 ]
 
--- draw/views.py
+++ draw/views.py
@@ -4,6 +4,22 @@ import json
 
 def index(request):
     return render(request, 'draw/index.html', {})
+  
+def one(request):
+    return render(request, 'draw/1.html')
+  
+def two(request):
+    return render(request, 'draw/2.html')
+  
+def three(request):
+    return render(request, 'draw/3.html')
+  
+def four(request):
+  return render(request, 'draw/4.html')
 
-def largescreen(request):
-    return render(request, 'draw/largescreen.html')
+def five(request):
+  return render(request, 'draw/5.html')
+
+def six(request):
+    return render(request, 'draw/6.html')
+  
